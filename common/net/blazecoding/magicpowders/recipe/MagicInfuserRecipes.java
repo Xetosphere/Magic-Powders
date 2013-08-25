90c1986ace9abc8321a6aeab785ddeaaacad9b86
Xetosphere <Xetosphere@gmail.com>
2013-08-26 01:50:39 +0200

    Fixed the recipes to work with metadata.

--- common/net/blazecoding/magicpowders/recipe/MagicInfuserRecipes.java
+++ common/net/blazecoding/magicpowders/recipe/MagicInfuserRecipes.java
@@ -1,5 +1,6 @@
 package net.blazecoding.magicpowders.recipe;
 
+import net.blazecoding.magicpowders.item.ModItems;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
@@ -8,14 +9,22 @@ public class MagicInfuserRecipes {
 	public MagicInfuserRecipes() {
 	}
 
-	public static ItemStack getSmeltingResult(int i, int j) {
-		return getOutput(i, j);
+	public static ItemStack getInfusingResult(int i, int j, int k, int l) {
+		return getOutput(i, j, k, l);
 	}
 
-	private static ItemStack getOutput(int i, int j) {
+	private static ItemStack getOutput(int i, int j, int k, int l) {
 
-		if (i == Item.coal.itemID && j == Item.ingotIron.itemID || i == Item.ingotIron.itemID && j == Item.coal.itemID) {
-			return new ItemStack(Item.ingotGold, 1);
+		if (i == ModItems.magicDust.itemID && k == 0 && j == Item.ingotIron.itemID && l == 0 || i == Item.ingotIron.itemID && k == 0 && j == ModItems.magicDust.itemID && l == 0) {
+			return new ItemStack(ModItems.magicIngot, 1, 0);
+		}
+		
+		if (i == ModItems.magicDust.itemID && k == 1 && j == Item.ingotIron.itemID && l == 0 || i == Item.ingotIron.itemID && k == 0 && j == ModItems.magicDust.itemID && l == 1) {
+			return new ItemStack(ModItems.magicIngot, 1, 1);
+		}
+		
+		if (i == ModItems.magicDust.itemID && k == 2 && j == Item.ingotIron.itemID && l == 0 || i == Item.ingotIron.itemID && k == 0 && j == ModItems.magicDust.itemID && l == 2) {
+			return new ItemStack(ModItems.magicIngot, 1, 2);
 		}
 
 		return null;
